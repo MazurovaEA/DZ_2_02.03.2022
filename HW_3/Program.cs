3a12531c96cadc5facf38a2dbead32569afa8f5f
Katerina <ealmazurova@internet.ru>
2022-03-02 21:20:00 +0300

    Задача 3 на вывод 3-ей цифры введенного числа

---
+++ HW_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число => ");
+string A = Console.ReadLine();
+int[] arr = new int[A.Length];
+for (int i = 0; i < A.Length; i++)
+{
+    arr[i] = Convert.ToInt32(A.Substring(i, 1));
+}
+if (arr.Length < 3) Console.WriteLine("Третьей цифры нет");
+else Console.WriteLine($"Третья цифра заданного числа => {arr[2]}");
