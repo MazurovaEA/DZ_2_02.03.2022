b937417f2a78836ef877b631f11bc26a40580334
Katerina <ealmazurova@internet.ru>
2022-03-02 21:55:20 +0300

    Задача 4 на определение выходного дня

---
+++ HW_4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите число, обозначающее день недели => ");
+string inputA = Console.ReadLine();
+int A = int.Parse(inputA);
+if (A > 7) Console.WriteLine("Неверно указан день недели: в неделе 7 дней");
+else
+if (A < 6) Console.WriteLine("Рабочий день");
+else Console.WriteLine("Выходной");
